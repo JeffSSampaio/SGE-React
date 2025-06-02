1a2b695ca8f84df475e063c83249e17920a70fe9
JeffSSampaio <jefferson.sampaio@cest.edu.br>
2025-06-02 10:48:33 -0300

    feat:criando rotas para os componentes

--- sgeprodutos/src/App.js
+++ sgeprodutos/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Estoque from './Estoque';
+import Login from './Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <nav>
+      <Link to="/">login</Link>
+      <Link to="/estoque">estoque</Link>
+     </nav>
+
+    <Routes>
+    <Route path="/" element={<Login/>}/>
+    <Route path="/estoque" element={<Estoque/>}/>
+    </Routes>
+
+   </Router>
   );
 }
 
