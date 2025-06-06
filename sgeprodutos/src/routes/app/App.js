d10e7d0c192b423836894e5de6c914675a79179e
JeffSSampaio <jefferson.sampaio@cest.edu.br>
2025-06-05 22:29:24 -0300

    fix:removendo  rota de login

--- sgeprodutos/src/routes/app/App.js
+++ sgeprodutos/src/routes/app/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import Estoque from '../estoque_route/Estoque';
-import Login from '../login_route/Login';
 import './App.css';
 
 function App() {
