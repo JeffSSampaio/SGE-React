338713b2bf91159bfaafa828e02f6ae80f533934
JeffSSampaio <jefferson.sampaio@cest.edu.br>
2025-06-05 22:07:37 -0300

    feat:adicionando botão de relatório.

--- sgeprodutos/src/routes/estoque_route/Estoque.js
+++ sgeprodutos/src/routes/estoque_route/Estoque.js
@@ -3,7 +3,8 @@ import ModalProduto from './ModalProduto';
 import CabecalhoEstoque from './cabecalhoEstoque';
 import TabelaEstoque from './TabelaEstoque';
 import imgAdd from '../imgs/imgAddProd.svg';
-import './cssestoque/estoque.css'; 
+import './cssestoque/estoque.css';
+import exportImg from '../imgs/botaoRell.svg'
 
 function Estoque() {
   // Carrega o estado inicial do localStorage
@@ -52,20 +53,51 @@ function Estoque() {
     }
   };
 
+  function exportarCSV() {
+  if (!produtos.length) {
+    alert("Nenhum produto para exportar!");
+    return;
+  }
+  const confirmarExportacao = window.confirm("Tem certeza que quer exportar o relatório do estoque?")
+if(confirmarExportacao){ 
+  const header = ["ID", "Nome", "Quantidade", "Unidade", "Código de Barras"];
+  const linhas = produtos.map(p =>
+    [p.id, p.nome, p.quantidade, p.unidade, p.codigoBarras].join(",")
+  );
+  const csvContent = [header.join(","), ...linhas].join("\n");
+
+  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", "Relatório-estoque-sgeprodutos.csv");
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+  }
+}
   return (
     <div className='bodyEstoque'>
       <CabecalhoEstoque />
       <div className='containerEstoque'>
-        <div className='containerTituloEstoque'>
+        <div className='linhaSuperior'>
           <h1 className='tituloTabela'>Estoque</h1>
-          <button className='butaoaddprod' onClick={abrirModalAdicionar}>
-            <img src={imgAdd} alt='adicionar produto' />
-          </button>
+          <div className="botoesSuperiores">
+            <button className='butaoaddprod' onClick={abrirModalAdicionar}>
+              <img src={imgAdd} alt='adicionar produto' />
+            </button>
+            <button className="butaoCsv" onClick={exportarCSV}>
+              <img src={exportImg} alt='Exportar Relatorio' />
+            </button>
+          </div>
         </div>
-        <TabelaEstoque 
-          produtos={produtos} 
-          onEditar={abrirModalEditar} 
-          onDeletar={deletarProduto} 
+
+        <TabelaEstoque
+          produtos={produtos}
+          onEditar={abrirModalEditar}
+          onDeletar={deletarProduto}
         />
       </div>
 
